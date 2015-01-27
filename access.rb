e62835fb5dc2346e1871b22bbdc871bb077b27ab
ishikawa <ee17093@meiji.ac.jp>
2015-01-27 15:18:53 +0900

    mechanize form access example

---
+++ access.rb
@@ -0,0 +1,18 @@
+#! ruby
+# -*- coding: utf-8 -*- 
+require 'nokogiri'
+require 'mechanize'
+agent = Mechanize.new
+agent.user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.99 Safari/537.36'
+page = agent.get('URL')
+#agent.page.encoding = "Shift_JIS" 
+#Shift_JISに変換すると送信する情報が変わってしまう
+#puts page.encoding 
+#puts page.body
+
+login_form = page.forms.first
+login_form.username = "username"
+login_form.password = "password"
+redirect_page = agent.submit(login_form)
+p redirect_page.body
+
